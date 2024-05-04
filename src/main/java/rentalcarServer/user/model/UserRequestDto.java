17231fe053e436aefec3fd238b6f5c132531fee2
k0gang <152839409+k0gang@users.noreply.github.com>
2024-05-05 01:41:08 +0900

    Add : userRequestDto , userResponseDto

---
+++ src/main/java/rentalcarServer/user/model/UserRequestDto.java
@@ -0,0 +1,60 @@
+package rentalcarServer.user.model;
+
+public class UserRequestDto {
+	private String userId;
+	private String userPassword;
+	private String userName;
+	private String userBirth;
+	private String userTelecom;
+	private String userPhone;
+	private boolean admin;
+	
+	public UserRequestDto() {
+
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserBirth() {
+		return userBirth;
+	}
+	public void setUserBirth(String userBirth) {
+		this.userBirth = userBirth;
+	}
+	public String getUserTelecom() {
+		return userTelecom;
+	}
+	public void setUserTelecom(String userTelecom) {
+		this.userTelecom = userTelecom;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	public boolean isAdmin() {
+		return admin;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	
+	
+}
---
+++ src/main/java/rentalcarServer/user/model/UserResponseDto.java
@@ -0,0 +1,91 @@
+package rentalcarServer.user.model;
+
+public class UserResponseDto {
+
+	private String userId;
+	private String userPassword;
+	private String userName;
+	private String userBirth;
+	private String userTelecom;
+	private String userPhone;
+	private boolean admin;
+
+	public UserResponseDto(String userId, String userPassword, String userName, String userBirth, String userTelecom,
+			String userPhone, boolean admin) {
+		super();
+		this.userId = userId;
+		this.userPassword = userPassword;
+		this.userName = userName;
+		this.userBirth = userBirth;
+		this.userTelecom = userTelecom;
+		this.userPhone = userPhone;
+		this.admin = admin;
+	}
+
+	public UserResponseDto(User user) {
+		this.userId = user.getUserId();
+		this.userPassword = user.getUserPassword();
+		this.userName = user.getUserName();
+		this.userBirth = user.getUserBirth();
+		this.userTelecom = user.getUserTelecom();
+		this.userPhone = user.getUserPhone();
+		this.admin = user.isAdmin();
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserBirth() {
+		return userBirth;
+	}
+
+	public void setUserBirth(String userBirth) {
+		this.userBirth = userBirth;
+	}
+
+	public String getUserTelecom() {
+		return userTelecom;
+	}
+
+	public void setUserTelecom(String userTelecom) {
+		this.userTelecom = userTelecom;
+	}
+
+	public String getUserPhone() {
+		return userPhone;
+	}
+
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
+}
